10b3ec7c2dc148a1667de6c851119cefbc46f958
LinderJK <slepchenkovia@gmail.com>
2024-04-10 18:35:03 +0300

    done getPolynom, memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,8 +92,15 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (args.length === 0) return null;
+  return function poly(x) {
+    let result = 0;
+    for (let i = 0; i < args.length; i += 1) {
+      result += args[i] * x ** (args.length - 1 - i);
+    }
+    return result;
+  };
 }
 
 /**
@@ -110,8 +117,15 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  return function memoized(...args) {
+    const key = JSON.stringify(args);
+    if (cache[key] === undefined) {
+      cache[key] = func.apply(this, args);
+    }
+    return cache[key];
+  };
 }
 
 /**
